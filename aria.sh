615712b9891cc3c16ef5f598ea9df004e8387f2a
MydriveBot <116956708+MydriveBot@users.noreply.github.com>
2022-11-01 08:44:44 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,4 +1,7 @@
+export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n\n' ',')
+export MAX_CONCURRENT_DOWNLOADS=5
+
 aria2c --enable-rpc --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --bt-max-peers=0 \
    --bt-stop-timeout=0 --min-split-size=10M --follow-torrent=mem --split=10 \
